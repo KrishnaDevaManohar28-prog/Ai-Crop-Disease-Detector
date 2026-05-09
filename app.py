3de73977a4818005864b1c8342a70db32d9365b0
ch.Krishna manohar <kingkrishna2877@gmail.com>
2026-05-09 18:28:43 +0530

    fixed railway port

--- app.py
+++ app.py
@@ -112,4 +112,8 @@ def home():
 # RUN APP
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
